7bf013ddd9a9ea64bf708d4ced7f48c7cecd2f27
Chris Eldredge <chris.eldredge@gmail.com>
2014-08-04 17:53:12 -0400

    Initial commit of plugin to run arbitrary external command as part of broccoli build.

---
+++ index.js
@@ -0,0 +1,61 @@
+var Writer = require('broccoli-writer')
+var childProcess = require('child_process')
+var Promise = require('rsvp').Promise
+
+module.exports = Exec
+Exec.prototype = Object.create(Writer.prototype)
+Exec.prototype.constructor = Exec
+function Exec (inputTree, options) {
+  if (!(this instanceof Exec)) return new Exec(inputTree, options)
+  this.inputTree = inputTree;
+  
+  for (var prop in options) {
+    this[prop] = options[prop];
+  }
+}
+
+Exec.prototype.write = function (readTree, destDir) {
+  var self = this
+
+  return readTree(this.inputTree).then(function (srcDir) {
+    return new Promise(function (resolve, reject) {
+      if (!self.command) {
+        reject('Must specify command to execute in options.command');
+        return;
+      }
+
+      var args = _replaceDestDirInArguments(self.args, destDir);
+      var execOptions = {
+        cwd: srcDir
+      };
+
+      var proc = childProcess.spawn(self.command, args, execOptions);
+
+      proc.on('close', function(code) {
+        if (code === 0) {
+          resolve();
+        } else {
+          reject(new Error(self.command + ' exited with code ' + code));
+        }
+      });
+
+      proc.stdout.on('data', function(data) {
+        self.log('' + data);
+      });
+
+      proc.stderr.on('data', function(data) {
+        self.log('' + data);
+      });
+    });
+  });
+}
+
+Exec.prototype.log = function (data) {
+  console.log('' + data);
+}
+
+function _replaceDestDirInArguments(args, destDir) {
+  return args.map(function(arg) {
+    return arg.replace('{destDir}', destDir);
+  });
+}
